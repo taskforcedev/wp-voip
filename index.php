7635427bbd4a9edea1146f361e5786ba9ca8ff07
PrivateSniper <privatesniper@gmail.com>
2016-05-15 14:51:51 +0100

    Update plugin meta

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 /*
 Plugin Name: VoIP Server Manager
-Plugin URI:  http://URI_Of_Page_Describing_Plugin_and_Updates
+Plugin URI:  https://github.com/taskforcedev/wp-voip
 Description: A plugin for managing voip servers and widgets.
 Version:     1.5
-Author:      John Smith
-Author URI:  http://tfstudio.uk
+Author:      PrivateSniper
+Author URI:  http://privatesniper.uk
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Domain Path: /languages
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
