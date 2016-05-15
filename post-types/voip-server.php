185ea0171a039614bf929c8f66209a8d802890ff
PrivateSniper <privatesniper@gmail.com>
2016-05-15 17:43:46 +0100

    Add server type selection in server details meta box

--- post-types/voip-server.php
+++ post-types/voip-server.php
@@ -20,11 +20,15 @@ add_action( 'init', 'voipServerManager_registerVoipServerPostType' );
 
 // Add meta box for VoIP Server Details.
 function voipServerManager_addMetaBoxes() {
-    add_meta_box ( 'voip-server-details-meta', 'Server Details', 'voipServerManager_ServerDetailsMeta', 'voip-server' );
+    add_meta_box ( 'voip-server-details-meta', 'Server Details', 'voipServerManager_ServerDetailsMeta', 'voip-server', 'normal' );
 }
 add_action( 'add_meta_boxes', 'voipServerManager_addMetaBoxes' );
 
 function voipServerManager_ServerDetailsMeta() {
+
+    // Server Type.
+    echo "<p><label>Server Type:</label> <select><option>TeamSpeak 3</option></select></p>";
+
     // Server Hostname - Default: Localhost
     echo "<p><label>Host:</label> <input type='text' /></p>";
 
