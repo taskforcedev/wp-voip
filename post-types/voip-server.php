0a01422448ba742153940c316590b67949d4a77a
PrivateSniper <privatesniper@gmail.com>
2016-05-15 17:35:55 +0100

    Start creating server details meta box

--- post-types/voip-server.php
+++ post-types/voip-server.php
@@ -1,5 +1,6 @@
 <?php
 
+// Create the VoIP Server post type.
 function voipServerManager_registerVoipServerPostType() {
     $args = array(
         'public' => true,
@@ -10,8 +11,23 @@ function voipServerManager_registerVoipServerPostType() {
         'labels' => array(
             'name'          => 'VoIP Servers',
             'name_singular' => 'VoIP Server',
-      )
+        ),
+        'register_meta_box_cb' => 'voipServerManager_addMetaBoxes'
     );
     register_post_type( 'voip-server', $args );
 }
 add_action( 'init', 'voipServerManager_registerVoipServerPostType' );
+
+// Add meta box for VoIP Server Details.
+function voipServerManager_addMetaBoxes() {
+    add_meta_box ( 'voip-server-details-meta', 'Server Details', 'voipServerManager_ServerDetailsMeta', 'voip-server' );
+}
+add_action( 'add_meta_boxes', 'voipServerManager_addMetaBoxes' );
+
+function voipServerManager_ServerDetailsMeta() {
+    // Server Hostname - Default: Localhost
+    echo "<p><label>Host:</label> <input type='text' /></p>";
+
+    // Server Password
+    echo "<p><label>Password:</label> <input type='password' /></p>";
+}
