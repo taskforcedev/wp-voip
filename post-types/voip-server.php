c2512c318ce49b8dff7a9bd5a2227d8de65fdda9
PrivateSniper <privatesniper@gmail.com>
2016-05-15 17:51:38 +0100

    Add Widget Preview meta box

--- post-types/voip-server.php
+++ post-types/voip-server.php
@@ -20,10 +20,12 @@ add_action( 'init', 'voipServerManager_registerVoipServerPostType' );
 
 // Add meta box for VoIP Server Details.
 function voipServerManager_addMetaBoxes() {
-    add_meta_box ( 'voip-server-details-meta', 'Server Details', 'voipServerManager_ServerDetailsMeta', 'voip-server', 'normal' );
+    add_meta_box ( 'voip-server-details-meta', 'Server Details', 'voipServerManager_ServerDetailsMeta', 'voip-server', 'normal', 'high' );
+    add_meta_box ( 'voip-widget-preview-meta', 'Widget Preview', 'voipServerManager_widgetPreviewMeta', 'voip-server', 'normal', 'low' );
 }
 add_action( 'add_meta_boxes', 'voipServerManager_addMetaBoxes' );
 
+// Server Details Meta Box.
 function voipServerManager_ServerDetailsMeta() {
 
     // Server Type.
@@ -34,4 +36,10 @@ function voipServerManager_ServerDetailsMeta() {
 
     // Server Password
     echo "<p><label>Password:</label> <input type='password' /></p>";
+}
+
+// Widget Preview Meta Box
+function voipServerManager_widgetPreviewMeta() {
+
+    echo '<p>This is an example of how this server would appear in the voip widget</p>';
 }
